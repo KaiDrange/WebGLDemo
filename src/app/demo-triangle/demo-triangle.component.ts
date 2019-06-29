bb5abca7ba60efb0ecd17f73267937d5f2faea38
Kai Drange <kaimdrange@hotmail.com>
2019-06-29 14:51:01 +0200

    Initial commit

---
+++ src/app/demo-triangle/demo-triangle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-triangle',
+  templateUrl: './demo-triangle.component.html',
+  styleUrls: ['./demo-triangle.component.sass']
+})
+export class DemoTriangleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
